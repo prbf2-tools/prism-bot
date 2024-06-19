4c0280c1431ddddd12e9dddac7facde670402067
cassius23 <emilekm@gmail.com>
2024-06-19 10:43:01 +0200

    Login to prism

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/prbf2-tools/prism-bot
 
 go 1.22.3
+
+require (
+	github.com/emilekm/go-prbf2 v0.0.0-20240614124638-23d882b0f371
+	github.com/goccy/go-yaml v1.11.3
+)
+
+require (
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"net"
+	"os"
 
+	"github.com/emilekm/go-prbf2/prism"
+	"github.com/prbf2-tools/prism-bot/internal/config"
+)
+
+func main() {
+	if err := run(os.Args[1:]); err != nil {
+		panic(err)
+	}
+}
+
+func run(args []string) error {
+	conf, err := config.NewConfig(args[0])
+	if err != nil {
+		return err
+	}
+
+	client, err := prism.Dial(net.JoinHostPort(conf.Host, conf.Port))
+	if err != nil {
+		return err
+	}
+
+	defer client.Close()
+
+	err = client.Login(context.TODO(), conf.Username, conf.Password)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
